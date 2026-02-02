28a2eab1f92f44613a30d4bf625210662c3c9270
Kento.Yamada <ymd65536@outlook.com>
2026-02-02 15:36:41 +0000

    Add parameterized test cases for addition using sample input data

---
+++ case_settings/test_var_cases.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+
+case_settings = {
+    "sample_case_1":(
+        {
+            "a": 1,
+            "b": 2
+        },
+        {
+            "result": 3
+        }
+    ),
+    "sample_case_2":(
+        {
+            "a": -1,
+            "b": 1
+        },
+        {
+            "result": 0
+        }
+    )
+}
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    list(case_settings.values()),
+    ids=list(case_settings.keys())
+)
+def test_sample_case(input_data, expected):
+    print("")
+    print(os.environ.get("PYTEST_CURRENT_TEST"))
+    a = input_data["a"]
+    b = input_data["b"]
+    result = a + b
+    assert result == expected["result"]
